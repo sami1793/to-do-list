ea860c40aabcf674d2687004f1f1afcaeb136eaa
sami1793 <sami_17_94@hotmail.com>
2023-03-21 23:13:31 -0300

    Se agrega preventDefault en formulario

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <Box bgGradient='linear(to-r,gray.700,gray.900)' w='100vw' minH='100vh' p={4}>
-      <Container className='App' maxW='md' p={10} borderRadius='2xl' spacing={4}  borderWidth={3} bg='white' borderColor='red.400'>
+      <Container className='App' maxW='md' p={10} pt={5} borderRadius='2xl' spacing={4}  borderWidth={3} bg='white' borderColor='red.400'>
         <Heading as='h1' size='3xl' textAlign='center' color='red.400' mb={10} >To Do List</Heading>
         <Box>
         <Form taskInput={taskInput} setTaskInput={setTaskInput} 
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,32 +1,34 @@
-import { FormControl, FormLabel, Input, FormErrorMessage, InputGroup, InputRightElement, Button, HStack } from '@chakra-ui/react'
-import { useState } from 'react'
+import { FormControl, Input, Box, FormErrorMessage, InputGroup, InputRightElement, Button, HStack } from '@chakra-ui/react'
 import { setLocalStorage } from '../../utils/localStorage';
 
-export const Form = ({taskInput, setTaskInput, taskList, setTaskList}) => {
+export const Form = ({ taskInput, setTaskInput, taskList, setTaskList }) => {
 
-    const addTask = () =>{
-        const newTaskId = taskList[taskList.length-1]['id']+1;
+    const addTask = (e) => {
+        e.preventDefault();
+        const newTaskId = taskList[taskList.length - 1]['id'] + 1;
         const newObjTask = {
             id: newTaskId,
             title: taskInput,
             done: false
         }
-        const newTaskList = [...taskList,newObjTask];
+        const newTaskList = [...taskList, newObjTask];
 
         setTaskList(newTaskList);
-        setLocalStorage('taskListStorage',newTaskList);
+        setLocalStorage('taskListStorage', newTaskList);
         setTaskInput('');
     }
 
     return (
-        <FormControl mb={5}>
-            <HStack>
-                <Input type='text' placeholder='Ingrese nueva tarea' bg='white' focusBorderColor='red.200' 
-                value={taskInput} onChange={(e)=>setTaskInput(e.target.value)}/>
-                <Button bg='gray.700' color='white' _hover={{bg:'gray.100', color:'pink.600'}}
-                onClick={addTask}>Agregar</Button>
-            </HStack>
-            <FormErrorMessage>El campo no puede estar vacío.</FormErrorMessage>
-        </FormControl>
+        <Box maxW={400} bg='gray.200' p={3} mb={5}>
+            <FormControl as='form'>
+                <HStack>
+                    <Input type='text' placeholder='Ingrese nueva tarea' bg='white' focusBorderColor='red.200'
+                        value={taskInput} onChange={(e) => setTaskInput(e.target.value)} />
+                    <Button type='submit' bg='gray.700' color='white' _hover={{ bg: 'gray.100', color: 'pink.600' }}
+                        onClick={addTask}>Agregar</Button>
+                </HStack>
+                <FormErrorMessage>El campo no puede estar vacío.</FormErrorMessage>
+            </FormControl>
+        </Box>
     )
 }
